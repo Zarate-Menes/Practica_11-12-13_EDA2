0457704a98c6204316ee574d3706e9d57f5a4128
Azzathoth1126 <alexis.mta2020@gmail.com>
2024-05-20 04:58:11 +0000

    codigo listo

--- Codigo/CodigoProporcionado/Actividad01/act05.c
+++ Codigo/CodigoProporcionado/Actividad01/act05.c
@@ -3,15 +3,18 @@
 
 int main(){
 
-    int tid, nth;
+    int tid; 
+    int nth;
 
-    #pragma omp parallel
+    #pragma omp parallel private(tid)
     {
+
         tid = omp_get_thread_num();
         nth = omp_get_num_threads();
         printf("Hola mundo desde el hilo %d de un total de %d\n", tid,nth);
     }
     printf("Adios\n");
+
     return 0;
 }
 
